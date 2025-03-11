26f46051b0eb259951dda7552b1365b19be7ae65
Gratia Gamage <32232745+GratiaSew@users.noreply.github.com>
2025-03-11 15:05:11 +0530

    Added Expandible feature to milestones

---
+++ js/smooth-transition.js
@@ -0,0 +1,9 @@
+document.getElementById('toggle-header').addEventListener('click', function() {
+  var content = document.getElementById('toggle-content');
+  
+  if (content.style.display === "none") {
+      content.style.display = "block";  // Show content
+  } else {
+      content.style.display = "none";   // Hide content
+  }
+});
